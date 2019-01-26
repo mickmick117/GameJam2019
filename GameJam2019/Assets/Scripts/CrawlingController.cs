e9baf33bdda67312379164f845eee360b5276ee9
Raphael Doye <raphaeldoye@gmail.com>
2019-01-26 07:21:06 -0500

    remove cinemachin

---
+++ GameJam2019/Assets/Scripts/CrawlingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlingController : MonoBehaviour
+{
+	public float speed = 5;
+	public Animator crawlingAnimator;
+	public Transform destination;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		//pMenu.transform.position = Vector3.MoveTowards(pMenu.transform.position, endPosition, speed * Time.deltaTime);
+	}
+
+	public void StartCrawling()
+	{
+		crawlingAnimator.SetBool("Start", true);
+		transform.position = Vector3.MoveTowards(transform.position, destination.position, speed * Time.deltaTime);
+	}
+}
