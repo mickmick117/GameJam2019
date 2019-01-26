1f2e76b1a5b9599159f9b83dc0f6eca3d92ac631
Michael <mickmick117@hotmail.com>
2019-01-26 14:43:07 -0500

    Aaaaa

---
+++ GameJam2019/Assets/Scripts/AppearanceTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearanceTrigger : MonoBehaviour
+{
+    public bool isTrigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isTrigger)
+        {
+            isTrigger = true;
+            public GameObject character;
+}
+    }
+}
