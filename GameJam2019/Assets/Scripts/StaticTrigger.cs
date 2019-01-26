a3b49056119953970ff096b0c8ddb0e3a8bfb5a9
Olivier <malec.olivier@gmail.com>
2019-01-25 22:55:34 -0500

    test trigger in progress

---
+++ GameJam2019/Assets/Scripts/StaticTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticTrigger : MonoBehaviour
+{
+    bool hasCollided = false;
+    public string referencetotext;
+
+    void OnTriggerEnter(Collider hit)
+    {
+        referencetotext = "Necesito mas comida";
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        referencetotext = "";
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        hasCollided = false;
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
