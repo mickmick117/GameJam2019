e4764bc25ffae55604ebebdad5465e18def898f1
Simon Bilski <AN51570@ens.etsmtl.ca>
2019-01-25 23:59:58 -0500

    interactTriggerZone

---
+++ GameJam2019/Assets/ScriptSimon/TriggerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerController : MonoBehaviour
+{
+    public Text txt;
+
+    public string textToShow;
+
+    private void Start()
+    {
+        txt.text = textToShow;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        txt.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        txt.gameObject.SetActive(false);
+    }
+}
