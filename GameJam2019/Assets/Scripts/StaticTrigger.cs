82763e563780c2ed6b33fcdf8706c38c81e10296
Unknown <lalonde.alexis@hotmail.com>
2019-01-25 23:15:42 -0500

    Auto stash before merge of "master" and "origin/master"

--- GameJam2019/Assets/Scripts/StaticTrigger.cs
+++ GameJam2019/Assets/Scripts/StaticTrigger.cs
@@ -15,15 +15,8 @@ public class StaticTrigger : MonoBehaviour
     void OnTriggerExit(Collider hit)
     {
         referencetotext = "";
-    }
-
-    void OnTriggerExit(Collider hit)
-    {
         hasCollided = false;
-
     }
-
-
 
     // Start is called before the first frame update
     void Start()
