09e65197870e55ab4a795dff5f0d39be474b8b4c
Michael <mickmick117@hotmail.com>
2019-01-27 01:31:22 -0500

    Intercative OBJ

---
+++ GameJam2019/Assets/OpenTheFkgTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTheFkgTrap : MonoBehaviour
+{
+    public Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light.isActiveAndEnabled)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
