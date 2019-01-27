42c777fdf1cabed3af6343a793aabdb2506bcd2b
Unknown <lalonde.alexis@hotmail.com>
2019-01-27 03:02:33 -0500

    Finito

---
+++ GameJam2019/Assets/Scripts/InteractiveChest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractiveChest : MonoBehaviour
+{
+    public GameObject chestToOpen;
+    public Text triggerEnterText;
+    public string triggerEnterPrompt;
+    public Renderer objectRenderer;
+
+    private bool isPlayerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TogglePlayerRangePrompt();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange)
+        {
+            if (TogglePlayerRangePrompt() && Input.GetAxis("Interact") > 0.0f)
+            {
+                chestToOpen.SetActive(false);
+                TogglePlayerRangePrompt();
+                GameManager.instance.OpenFinalRoom();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isPlayerInRange = true;
+        TogglePlayerRangePrompt();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isPlayerInRange = false;
+        TogglePlayerRangePrompt();
+    }
+
+    private bool TogglePlayerRangePrompt()
+    {
+        bool isActiveAndInRange = isPlayerInRange && chestToOpen.activeSelf && objectRenderer.isVisible && Inventory.instance.items.Count == 6;
+
+        triggerEnterText.text = (isActiveAndInRange) ? triggerEnterPrompt : "";
+
+        return isActiveAndInRange;
+    }
+}
