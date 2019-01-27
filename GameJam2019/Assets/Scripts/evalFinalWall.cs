099642620afd98736c7ede59977520c6be7c0f5b
Olivier <malec.olivier@gmail.com>
2019-01-27 00:11:32 -0500

    resolve conflict

---
+++ GameJam2019/Assets/Scripts/evalFinalWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evalFinalWall : MonoBehaviour
+{
+    public int maxInv;
+    private Inventory inv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inv = Inventory.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inv.itemKeys.Count >= maxInv)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
